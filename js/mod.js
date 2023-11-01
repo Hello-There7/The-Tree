6f517dd80e08004dc1eddf5e7ffc0b739a749b39
Hello-There7 <147861793+Hello-There7@users.noreply.github.com>
2023-11-01 16:45:21 -0500

    Update mod.js

--- js/mod.js
+++ js/mod.js
@@ -41,8 +41,9 @@ function canGenPoints(){
 function getPointGen() {
 	if(!canGenPoints())
 		return new Decimal(0)
-		if (hasUpgrade('k', 11)) gain = gain.times(2)
+		
 	let gain = new Decimal(1)
+	if (hasUpgrade('k', 11)) gain = gain.times(2)
 	return gain
 }
 
