b7cc255768feed55b691e1820bf1a1c04fcff2f2
Hello-There7 <147861793+Hello-There7@users.noreply.github.com>
2023-11-01 16:42:32 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -6,7 +6,7 @@ addLayer("k", {
         unlocked: true,
 		points: new Decimal(0),
     }},
-    color: "#222222",
+    color: "#000000",
     requires: new Decimal(10), // Can be a function that takes requirement increases into account
     resource: "Black", // Name of prestige currency
     baseResource: "Darkness", // Name of resource prestige is based on
