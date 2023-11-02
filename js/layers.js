0c49671e585a0735ee7feacf8e5ec9290c469aae
Hello-There7 <147861793+Hello-There7@users.noreply.github.com>
2023-11-02 18:38:39 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -23,9 +23,6 @@ addLayer("k", {
         return new Decimal(1)
     },
     row: 0, // Row the layer is in on the tree (0 is the first row)
-    hotkeys: [
-        {key: "p", description: "P: Reset for prestige points", onPress(){if (canReset(this.layer)) doReset(this.layer)}},
-    ],
     layerShown(){return true},
     upgrades: {
         11: {
@@ -68,6 +65,45 @@ addLayer("k", {
                 return player[this.layer].points.add(1).pow(0.5)
             },
             effectDisplay() { return format(upgradeEffect(this.layer, this.id))+"x" },
+        },
+    },
+})
+addLayer("r", {
+    name: "r", // This is optional, only used in a few places, If absent it just uses the layer id.
+    symbol: "R", // This appears on the layer's node. Default is the id with the first letter capitalized
+    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    startData() { return {
+        unlocked: true,
+		points: new Decimal(0),
+    }},
+    color: "#ff0000",
+    requires: new Decimal(1000), // Can be a function that takes requirement increases into account
+    resource: "Red", // Name of prestige currency
+    baseResource: "Black", // Name of resource prestige is based on
+    baseAmount() {return player.k.points}, // Get the current amount of baseResource
+    type: "normal", // normal: cost to gain currency depends on amount gained. static: cost depends on how much you already have
+    exponent: 0, // Prestige currency exponent
+    gainMult() { // Calculate the multiplier for main currency from bonuses
+        mult = new Decimal(1)
+        if(hasMilestone(r, 0)) mult = mult.times(2)
+        if(hasMilestone(r, 1)) mult = mult.times(2)
+        return mult
+    },
+    gainExp() { // Calculate the exponent on main currency from bonuses
+        return new Decimal(1)
+    },
+    row: 1, // Row the layer is in on the tree (0 is the first row)
+    layerShown(){return true},
+    milestones: {
+        0: {
+            requirementDescription: "#FF0000",
+            effectDescription: "",
+            done() { return player.r.points.gte(1) },
+        },
+        1: {
+            requirementDescription: "Fire Essence",
+            effectDescription: "",
+            done() { return player.r.points.gte(5) },
         },
     },
 })
