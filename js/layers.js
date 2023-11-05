890626dae84ee79ba7130e0622d26affc23eaa53
Hello-There7 <147861793+Hello-There7@users.noreply.github.com>
2023-11-04 21:17:07 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -160,7 +160,7 @@ addLayer("e", {
 })
 addLayer("u", {
     name: "u", // This is optional, only used in a few places, If absent it just uses the layer id.
-    position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
+    position: 1, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     symbol: "⇮", // This appears on the layer's node. Default is the id with the first letter capitalized
     startData() { return {
         unlocked: true,
@@ -180,7 +180,7 @@ addLayer("u", {
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
-    row: 1, // Row the layer is in on the tree (0 is the first row)
+    row: 0, // Row the layer is in on the tree (0 is the first row)
     layerShown(){return true},
     upgrades: {
         11: {
