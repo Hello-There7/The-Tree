046076d5a1fbaffe54f4945ea29a121b3802d908
Hello-There7 <147861793+Hello-There7@users.noreply.github.com>
2023-12-23 17:46:58 -0600

    b

--- js/layers.js
+++ js/layers.js
@@ -71,7 +71,7 @@ addLayer("b", {
     gainExp() { // Calculate the exponent on main currency from bonuses
         return new Decimal(1)
     },
-    row: 0, // Row the layer is in on the tree (0 is the first row)
+    row: 1, // Row the layer is in on the tree (0 is the first row)
     layerShown(){return true},
     upgrades: {
         11: {
