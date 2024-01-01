7cef06f71233300b110944af654d2f2870e067cc
Hello-There7 <147861793+Hello-There7@users.noreply.github.com>
2024-01-01 13:49:15 -0600

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -2,7 +2,6 @@ addLayer("a", {
     name: "a", // This is optional, only used in a few places, If absent it just uses the layer id.
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     symbol: "A", // This appears on the layer's node. Default is the id with the first letter capitalized
-    branches: [],
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
@@ -48,7 +47,6 @@ addLayer("b", {
     name: "b", // This is optional, only used in a few places, If absent it just uses the layer id.
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     symbol: "B", // This appears on the layer's node. Default is the id with the first letter capitalized
-    branches: [a],
     startData() { return {
         unlocked: true,
 		points: new Decimal(0),
