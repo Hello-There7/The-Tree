7e1d6c840635a9c994d163dc27ef8da3c9a3abb8
Hello-There7 <147861793+Hello-There7@users.noreply.github.com>
2025-04-21 12:42:59 -0500

    PLEASE LET THIS WORK

--- js/layers.js
+++ js/layers.js
@@ -1,5 +1,5 @@
 addLayer("a", {
-    name: "a", // This is optional, only used in a few places, If absent it just uses the layer id.
+    name: "Antimatter", // This is optional, only used in a few places, If absent it just uses the layer id.
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     symbol: "A", // This appears on the layer's node. Default is the id with the first letter capitalized
     startData() { return {
@@ -73,7 +73,7 @@ addLayer("a", {
          11: {
             title: "A",
             cost(x) { return new Decimal(10).pow(x.mul(0.2).add(1)) },
-            display() { return `You have ${ player.a.buyables[11] } of this buyable, which multiplies point gain by ${player.a.buyables[11].add(1)}` },
+            display() { return `You have ${ a.buyables[11] } of this buyable, which multiplies point gain by ${a.buyables[11].add(1)}` },
             canAfford() { return player[this.layer].points.gte(this.cost()) },
             buy() {
                 player[this.layer].points = player[this.layer].points.sub(this.cost())
