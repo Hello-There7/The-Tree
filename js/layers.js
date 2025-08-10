28d05dae36a1b8d614d3aaba15682b97d2c3a9c5
Hello-There7 <147861793+Hello-There7@users.noreply.github.com>
2025-08-09 19:33:25 -0500

    Update layers.js

--- js/layers.js
+++ js/layers.js
@@ -1,4 +1,4 @@
-addLayer("C", {
+addLayer("BK ", {
     name: "Choice", // This is optional, only used in a few places, If absent it just uses the layer id.
     position: 0, // Horizontal position within a row. By default it uses the layer id and sorts in alphabetical order
     symbol: "⌗", // This appears on the layer's node. Default is the id with the first letter capitalized
